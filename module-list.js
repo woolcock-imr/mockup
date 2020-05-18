e3aafc165497763d796346d1dc750eb01d6cb105
Zhiming Zhou <zzm.vmiis@gmail.com>
2020-05-18 15:23:48 +1000

    update

--- module-list.js
+++ module-list.js
@@ -5,8 +5,12 @@
         "panel-main":  	        {url:"$H/m/panel-main.html",router:1},
         "form-field":  	        {url:"$H/m/form-field.html",router:1},
         "form-logic":  	        {url:"$H/m/form-logic.html",router:1},
-        "tb001-data":  	        {url:"$H/m/tb001-data.html",Table:"demo-2020042001",form_module:"tb001-form",router:1},
-        "tb001-form":  	        {url:"$H/m/tb001-form.html",Table:"demo-2020042001"},
+        "hans-scoring-data":    {url:"$H/m/hans-scoring-data.html",Table:"demo-2020042001",form_module:"hans-scoring-form",router:1},
+        "hans-scoring-form":    {url:"$H/m/hans-scoring-form.html",Table:"demo-2020042001"},
+        "phq-9-english-data":   {url:"$H/m/phq-9-english-data.html",Table:"demo-2020051801",form_module:"phq-9-english-form",router:1},
+        "phq-9-english-form":   {url:"$H/m/phq-9-english-form.html",Table:"demo-2020051801"},
+
+        
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
